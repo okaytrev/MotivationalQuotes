ba6e25608306596a696711500258556e0655702d
trevar.hupf <trevabooo@gmail.com>
2021-10-15 12:29:12 -0600

    Twilio api call, send rando motivational quote to text, this is private and shows api key, free trial of twilio

--- motivation.py
+++ motivation.py
@@ -1,8 +1,8 @@
-from math import inf
 import requests
 import json
 from random import randint
-
+from twilio.rest import Client 
+ 
 def random_quote():
     api_response = requests.get("https://type.fit/api/quotes") #api into variable
     data = api_response.text  #store api response
@@ -11,4 +11,11 @@ def random_quote():
     info = parse_json [random_index]['text'] #pulls the random quote
     return ("Inspirational quote of the day: " + info)
 
-print(random_quote())
+
+account_sid = 'ACf4990cb06cc8d26bb53480f12a81ed33' 
+auth_token = 'dfcef6c2079c3b160d06fe2f78520b34' 
+client = Client(account_sid, auth_token) 
+ 
+message = client.messages.create(messaging_service_sid='MG9bb4c52f16884d4a1981702ba479dcb2', body= random_quote(), to='+13035185767' ) 
+ 
+print(message.sid)
